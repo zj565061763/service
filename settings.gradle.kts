49fbcccd8764c5951525844edb0bca1430861a20
zj565061763 <565061763@qq.com>
2023-01-15 21:18:41 +0800

    refactor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,6 @@
 enableFeaturePreview("VERSION_CATALOGS")
 
 pluginManagement {
-    val gradlePluginVersion: String by settings
-    val kotlinVersion: String by settings
-    val kspVersion: String by settings
-
-    plugins {
-        id("com.android.application") version gradlePluginVersion apply false
-        id("com.android.library") version gradlePluginVersion apply false
-        id("org.jetbrains.kotlin.android") version kotlinVersion apply false
-        kotlin("jvm") version kotlinVersion apply false
-        id("com.google.devtools.ksp") version kspVersion apply false
-    }
-
     repositories {
         gradlePluginPortal()
         google()
@@ -30,7 +18,6 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "service"
-
 include(":app")
 include(":lib")
 include(":app_module_a")
