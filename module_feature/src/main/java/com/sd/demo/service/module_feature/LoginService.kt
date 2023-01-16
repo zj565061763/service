1e36a50aa691424f40f123ab93f5ff11e814db3b
zj565061763 <565061763@qq.com>
2023-01-17 01:24:08 +0800

    rename

--- module_feature/src/main/java/com/sd/demo/service/module_feature/LoginService.kt
+++ module_feature/src/main/java/com/sd/demo/service/module_feature/LoginService.kt
@@ -4,15 +4,15 @@ import com.sd.demo.service.app_module_common.LoginService
 import com.sd.demo.service.app_module_common.logMsg
 import com.sd.lib.service.FServiceImpl
 
-@FServiceImpl("ModuleA1")
-class LoginServiceModuleA1 : LoginService {
+@FServiceImpl("ModuleFeature1")
+class LoginServiceModuleFeature1 : LoginService {
     override fun login() {
         logMsg { "$this login" }
     }
 }
 
-@FServiceImpl("ModuleA2")
-class LoginServiceModuleA2 : LoginService {
+@FServiceImpl("ModuleFeature2")
+class LoginServiceModuleFeature2 : LoginService {
     override fun login() {
         logMsg { "$this login" }
     }
