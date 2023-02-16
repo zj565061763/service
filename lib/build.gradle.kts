b8057dfde0862baa22617bb06b8af6f3509e580e
zj565061763 <565061763@qq.com>
2023-02-16 14:36:39 +0800

    1.0.0-alpha11

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "service"
-val libVersionName = "1.0.0-alpha10"
+val libVersionName = "1.0.0-alpha11"
 
 android {
     namespace = "com.sd.lib.service"
