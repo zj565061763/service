8adea907396ba6d1d02876e7057e9ea4231f2e06
zj565061763 <565061763@qq.com>
2023-05-24 20:17:23 +0800

    1.0.0-alpha15

--- service-compiler/build.gradle.kts
+++ service-compiler/build.gradle.kts
@@ -27,7 +27,7 @@ afterEvaluate {
                 from(components["java"])
                 groupId = "com.sd.lib.android"
                 artifactId = "service-compiler"
-                version = "1.0.0-alpha14"
+                version = "1.0.0-alpha15"
             }
         }
     }
