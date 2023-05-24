030dc3ed3180becb50629626a9ce8002484478d6
zj565061763 <565061763@qq.com>
2023-05-24 20:13:24 +0800

    format

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -27,16 +27,16 @@ android {
         }
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    buildFeatures {
-        viewBinding = true
     }
 }
 
