b7ff9f29e96352c47b615584e3e0a34e6a8fbb5d
zjun <565061763@qq.com>
2024-06-30 18:11:40 +0800

    comment

--- lib/src/main/java/com/sd/lib/service/FService.kt
+++ lib/src/main/java/com/sd/lib/service/FService.kt
@@ -15,7 +15,8 @@ inline fun <reified T> fs(name: String = ""): T {
 }
 
 /**
- * 创建[T]的实现类对象
+ * 注册[T]，并把[T]和他所有实现的接口绑定，
+ * 当外部获取接口实例时，会调用[T]的无参构造方法创建对象返回
  */
 inline fun <reified T> fsRegister() {
     return FS.register(T::class.java)
