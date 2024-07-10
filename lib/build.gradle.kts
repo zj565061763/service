e00c4238221acaaa188cac6326d4cbcd7eeed333
zjun <565061763@qq.com>
2024-07-11 00:50:51 +0800

    1.1.0-alpha05

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "service"
-val libVersion = "1.1.0-alpha04"
+val libVersion = "1.1.0-alpha05"
 
 android {
     namespace = "com.sd.lib.service"
